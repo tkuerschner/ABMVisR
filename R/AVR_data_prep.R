362391547a8b1c833a154e9647804bff65481c06
tkuerschner <tobias.kuerschner@googlemail.com>
2024-08-15 13:21:29 +0200

    Added test case

--- R/AVR_data_prep.R
+++ R/AVR_data_prep.R
@@ -19,6 +19,12 @@ summarize_simulation_data <- function(data,
                     target_cols,
                     group_cols = NULL) {
   library(dplyr)
+  #REMOVE ME
+    data_by_id <- NULL
+    summary_by_scenario <- NULL
+    summary_by_group <- NULL
+    summary_by_scenario_and_group <- NULL
+  #REMOVE ME
   if (data_type == "count") {
   cols_to_keep <- c(id_col, time_col, target_cols, group_cols, scenario_col)
   if (!is.null(id_col)) {
@@ -102,3 +108,52 @@ summarize_simulation_data <- function(data,
         summary_by_group = summary_by_group,
         summary_by_scenario_and_group = summary_by_scenario_and_group))
 }
+
+
+#REMOVE ME
+count_data <- read.csv("test_data/wolf_model_spatial_data/Scenario1_36x_run1.csv", header = TRUE, skip = 6)
+
+count_data <- count_data %>% 
+  group_by(runName) %>%
+  mutate(grouptester = sample(c("A", "B", "C"), 1)) %>%
+  mutate(scenarioTester = sample(c("scenarioA", "scenarioB"), 1)) %>%
+  ungroup()
+
+
+#(count_data %>% filter(runName == "scenario1_77337344_typeOfCore_incMort_0") )$grouptester
+#
+#
+#head(count_data)
+#head(as.data.frame(count_data), 20)
+#
+#unique(count_data$runName)
+
+# Example of using the function with count data
+summarized_counts <- summarize_simulation_data(
+  data = count_data, 
+  data_type = "count",
+  time_col = "X.step.",
+  id_col = "runName",
+  scenario_col = "scenarioTester",
+  target_cols = c("count.turtles", "totalInd", "reproMales", "reproFemales")#,
+  #group_cols = c("grouptester")
+)
+
+print(summarized_counts)
+colnames(summarized_counts)
+
+individual_data <- read.csv("test_data/asf_abm_data/all_groups_400.csv", header = TRUE)
+# Example of using the function with individual data
+summarized_individuals <- summarize_simulation_data(
+  data = individual_data,
+  data_type = "individual",
+  time_col = "iteration",
+  id_col = NULL,  # No id_col
+  scenario_col = NULL,
+  target_cols = NULL,        # Not needed for individual type
+  group_cols = c("age_class", "sex") # Group by age_class
+)
+
+print(summarized_individuals)
+
+#REMOVE ME
